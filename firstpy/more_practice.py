1728fee3fff73d7c41ee99a46415442cf3caf4c2
ariz-atalgo <ariz@atalgo.com>
2026-07-30 12:30:16 +0530

    Add item creation endpoint to FastAPI application

---
+++ firstpy/more_practice.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/items")
+def create_item(item: Item):
+    
+    new_item = {
+        "name": item.name,
+        "description": item.description,
+        "price": item.price,
+        "tax": item.tax
+    }
+
+    return new_item
