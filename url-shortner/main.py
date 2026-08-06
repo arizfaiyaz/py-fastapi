bea5c6f05ee1771f13258e0a15d4c8f1c747baa7
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-08-06 23:32:52 +0530

    Refactor FastAPI application setup and enhance URL model with request/response schemas

--- url-shortner/main.py
+++ url-shortner/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, HttpUrl
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
-from sqlalchemy import column, create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker, declarative_base, session
+from sqlalchemy import column, create_engine, Integer, String
 from fastapi.responses import RedirectResponse
 import string, random
 
@@ -22,7 +22,24 @@ class URL(Base):
     __tablename__ = "urls"
     id = column(Integer, primary_key=True, index=True)
     original_url = column(String, unique=True, nullable=False)
+    short_code = column(String, unique=True, index=True, nullable=False)
     
+Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+class URLRequest(BaseModel):
+    original_url: HttpUrl
+
+class URLResponse(BaseModel):
+    original_url: str
+    short_code: str
+    short_url: str
+    
+    class COnfig:
+        orm_mode = True
+
+def generate_short_code(length = 6):
+    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+app = FastAPI(title="URL Shortener", description="A simple URL shortener API", version="1.0.0")
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
